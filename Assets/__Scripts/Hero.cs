dca7bd394953ba766c5f1a1845417365d62aa32e
Albert Peng <albertpeng96@yahoo.com>
2017-10-23 01:47:04 -0400

    Added Hero.Update()

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -2,49 +2,35 @@
 using System.Collections;
 
 public class Hero : MonoBehaviour {
+    static public Hero      S; // Singleton
 
-	static public Hero		S;
+    // These fields control the movement of the ship
+    public float            speed = 30;
+    public float            rollMult = -45;
+    public float            pitchMult = 30;
 
-	public float	speed = 30;
-	public float	rollMult = -45;
-	public float  	pitchMult=30;
+    // Ship status information
+    public float            shieldLevel = 1;
 
-	public float	shieldLevel=1;
+    public bool ____________________________;
 
-	public bool	_____________________;
-	public Bounds bounds;
+    void Awake() {
+        S = this;  // Set the Singleton
+    }
 
-	void Awake(){
-		S = this;
-		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
-	}
+    void Update () {
+        // Pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal");                          // 1
+        float yAxis = Input.GetAxis("Vertical");                            // 1
 
+        // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float xAxis = Input.GetAxis("Horizontal");
-		float yAxis = Input.GetAxis("Vertical");
-
-		Vector3 pos = transform.position;
-		pos.x += xAxis * speed * Time.deltaTime;
-		pos.y += yAxis * speed * Time.deltaTime;
-		transform.position = pos;
-		
-		bounds.center = transform.position;
-		
-		// constrain to screen
-		Vector3 off = Utils.ScreenBoundsCheck(bounds,BoundsTest.onScreen);
-		if (off != Vector3.zero) {  // we need to move ship back on screen
-			pos -= off;
-			transform.position = pos;
-		}
-		
-		// rotate the ship to make it feel more dynamic
-		transform.rotation =Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult,0);
-	}
+        // Rotate the ship to make it feel more dynamic                     // 2
+        transform.rotation = Quaternion.Euler(yAxis*pitchMult,xAxis*rollMult,0);
+    }
 }
+
